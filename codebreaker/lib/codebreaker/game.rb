2aeef341ceed85fbad08e8c6f007c5d29a192eb4
homeway <homeway.xue@gmail.com>
2013-10-25 23:27:50 +0800

    完成第9章

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -6,10 +6,28 @@ module Codebreaker
       @output = output
     end
     def start(secret)
+      @secret = secret
       @output.puts '欢迎来玩破译密码的游戏！'
       @output.puts '请从1-6的数字中选择4个作为猜想答案：'
     end
     def guess(guess)
+      mark1, mark2 = '', ''
+      (0..3).each do |index|
+        if exact_match?(guess, index)
+          mark1 << '+'
+        elsif number_match?(guess, index)
+          mark2 << '-'
+        end
+      end
+      @output.puts mark1 +mark2
+    end
+
+    def exact_match?(guess, index)
+      guess[index] == @secret[index]
+    end
+
+    def number_match?(guess, index)
+      @secret.include?(guess[index])
     end
   end
 end
--- codebreaker/spec/codebreaker/game_spec.rb
+++ codebreaker/spec/codebreaker/game_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 
 module Codebreaker
   describe Game do
-    describe "#start" do
-      let(:output){ double('output').as_null_object }
-      let(:game){ Game.new(output) }
+    let(:output){ double('output').as_null_object }
+    let(:game){ Game.new(output) }
 
+    describe "#start" do
       it "发出欢迎消息" do
         output.should_receive(:puts).with('欢迎来玩破译密码的游戏！')
         game.start('1234')
@@ -15,6 +15,30 @@ module Codebreaker
       it "游戏提示" do
         output.should_receive(:puts).with('请从1-6的数字中选择4个作为猜想答案：')
         game.start('1234')
+      end
+    end
+
+    describe "#guess" do
+      context "没有匹配" do
+        it "返回mark为空字符串''" do
+          game.start('1234')
+          output.should_receive(:puts).with('')
+          game.guess('5555')
+        end
+      end
+      context "1个匹配" do
+        it "返回mark为'-'" do
+          game.start('1234')
+          output.should_receive(:puts).with('-')
+          game.guess('2555')
+        end
+      end
+      context "1个匹配" do
+        it "返回mark为'+'" do
+          game.start('1234')
+          output.should_receive(:puts).with('+')
+          game.guess('1555')
+        end
       end
     end
   end

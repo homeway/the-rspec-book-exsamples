54814d23233c7e61e13706f591166313cd1bb1d9
homeway <homeway.xue@gmail.com>
2013-10-26 09:52:55 +0800

    收尾，可以玩了

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -5,11 +5,20 @@ module Codebreaker
     def initialize(output)
       @output = output
     end
+    def generate
+      code = ''
+      (0..3).each { code << "#{rand(6)+1}" }
+      puts 'the code had generate: '
+      puts '****'
+      return code
+    end
+
     def start(secret)
       @secret = secret
       @output.puts '欢迎来玩破译密码的游戏！'
       @output.puts '请从1-6的数字中选择4个作为猜想答案：'
     end
+
     def guess(guess)
       mark1, mark2 = '', ''
       (0..3).each do |index|
@@ -19,7 +28,9 @@ module Codebreaker
           mark2 << '-'
         end
       end
-      @output.puts mark1 +mark2
+      mark = mark1 +mark2
+      @output.puts mark
+      return mark
     end
 
     def exact_match?(guess, index)
